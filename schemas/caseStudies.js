3ff8947bcee7db7bc4cd13c65c1a059a3f8de154
Sina <visualsbysina@gmail.com>
2023-03-11 20:31:58 -0500

    Update caseStudies schema

--- schemas/caseStudies.js
+++ schemas/caseStudies.js
@@ -9,9 +9,39 @@ export default {
       type: 'string',
     },
     {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+    },
+    {
       name: 'body',
       title: 'Body',
-      type: 'text',
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+          styles: [
+            { title: 'Normal', value: 'normal' },
+            { title: 'Heading 1', value: 'h1' },
+            { title: 'Heading 2', value: 'h2' },
+            { title: 'Heading 3', value: 'h3' },
+            { title: 'Heading 4', value: 'h4' },
+            { title: 'Heading 5', value: 'h5' },
+            { title: 'Heading 6', value: 'h6' },
+            { title: 'Bold', value: 'bold' },
+            { title: 'Italic', value: 'italic' },
+          ],
+          marks: {
+            decorators: [{ title: 'Strong', value: 'strong' }, { title: 'Emphasis', value: 'em' }],
+          },
+        },
+        {
+          type: 'image',
+          options: {
+            hotspot: true,
+          },
+        },
+      ],
     },
   ],
 }
