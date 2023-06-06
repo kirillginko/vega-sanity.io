945c1ad36284bc9de0ac7d28251609d809a35bb7
Sina <visualsbysina@gmail.com>
2023-06-05 23:30:09 -0400

    new schemas

--- schemas/about.js
+++ schemas/about.js
@@ -1,48 +1,81 @@
 export default {
-    name: 'about',
-    title: 'About',
-    type: 'document',
-    fields: [
-      {
-        name: 'header',
-        title: 'Header',
-        type: 'string',
-      },
-      {
-        name: 'image',
-        title: 'Image',
-        type: 'image',
-      },
-      {
-        name: 'body',
-        title: 'Body',
-        type: 'array',
-        of: [
-          {
-            type: 'block',
-            styles: [
-              { title: 'Normal', value: 'normal' },
-              { title: 'Heading 1', value: 'h1' },
-              { title: 'Heading 2', value: 'h2' },
-              { title: 'Heading 3', value: 'h3' },
-              { title: 'Heading 4', value: 'h4' },
-              { title: 'Heading 5', value: 'h5' },
-              { title: 'Heading 6', value: 'h6' },
-              { title: 'Bold', value: 'bold' },
-              { title: 'Italic', value: 'italic' },
-            ],
-            marks: {
-              decorators: [{ title: 'Strong', value: 'strong' }, { title: 'Emphasis', value: 'em' }],
-            },
-          },
-          {
-            type: 'image',
-            options: {
-              hotspot: true,
-            },
-          },
-        ],
-      },
-    ],
-  }
+	name: 'about',
+	title: 'About',
+	type: 'document',
+	fields: [
+	  {
+		name: 'header',
+		title: 'Header',
+		type: 'string',
+	  },
+	  {
+		name: 'imagesGallery',
+		title: 'Image Gallery',
+		type: 'array',
+		of: [{ type: 'image' }]
+	   },
+	  {
+		name: 'body',
+		title: 'Body',
+		type: 'array',
+		of: [
+		  {
+			type: 'block',
+			styles: [
+			  { title: 'Normal', value: 'normal' },
+			  { title: 'Heading 1', value: 'h1' },
+			  { title: 'Heading 2', value: 'h2' },
+			  { title: 'Heading 3', value: 'h3' },
+			  { title: 'Heading 4', value: 'h4' },
+			  { title: 'Heading 5', value: 'h5' },
+			  { title: 'Heading 6', value: 'h6' },
+			  { title: 'Bold', value: 'bold' },
+			  { title: 'Italic', value: 'italic' },
+			],
+			marks: {
+			  decorators: [
+				{ title: 'Strong', value: 'strong' },
+				{ title: 'Emphasis', value: 'em' },
+			  ],
+			},
+		  },
+		],
+	  },
+	  {
+		name: 'imagesGallery2',
+		title: 'Image Gallery 2',
+		type: 'array',
+		of: [{ type: 'image' }]
+	   },
+	  {
+		name: 'body2',
+		title: 'Body2',
+		type: 'array',
+		of: [
+		  {
+			type: 'block',
+			styles: [
+			  { title: 'Normal', value: 'normal' },
+			  { title: 'Heading 1', value: 'h1' },
+			  { title: 'Heading 2', value: 'h2' },
+			  { title: 'Heading 3', value: 'h3' },
+			  { title: 'Heading 4', value: 'h4' },
+			  { title: 'Heading 5', value: 'h5' },
+			  { title: 'Heading 6', value: 'h6' },
+			  { title: 'Bold', value: 'bold' },
+			  { title: 'Italic', value: 'italic' },
+			],
+			marks: {
+			  decorators: [
+				{ title: 'Strong', value: 'strong' },
+				{ title: 'Emphasis', value: 'em' },
+			  ],
+			},
+		  },
+		],
+	  },
+	],
+  };
+  
+
   
--- schemas/caseStudies.js
+++ schemas/caseStudies.js
@@ -1,47 +1,83 @@
 export default {
-  name: 'caseStudies',
-  title: 'Case Studies',
-  type: 'document',
-  fields: [
-    {
-      name: 'header',
-      title: 'Header',
-      type: 'string',
-    },
-    {
-      name: 'image',
-      title: 'Image',
-      type: 'image',
-    },
-    {
-      name: 'body',
-      title: 'Body',
-      type: 'array',
-      of: [
-        {
-          type: 'block',
-          styles: [
-            { title: 'Normal', value: 'normal' },
-            { title: 'Heading 1', value: 'h1' },
-            { title: 'Heading 2', value: 'h2' },
-            { title: 'Heading 3', value: 'h3' },
-            { title: 'Heading 4', value: 'h4' },
-            { title: 'Heading 5', value: 'h5' },
-            { title: 'Heading 6', value: 'h6' },
-            { title: 'Bold', value: 'bold' },
-            { title: 'Italic', value: 'italic' },
-          ],
-          marks: {
-            decorators: [{ title: 'Strong', value: 'strong' }, { title: 'Emphasis', value: 'em' }],
-          },
-        },
-        {
-          type: 'image',
-          options: {
-            hotspot: true,
-          },
-        },
-      ],
-    },
-  ],
-}
+	name: 'caseStudies',
+	title: 'Case Studies',
+	type: 'document',
+	fields: [
+	  {
+		name: 'header',
+		title: 'Header',
+		type: 'string',
+	  },
+	  {
+		name: 'image',
+		title: 'Image',
+		type: 'image',
+		options: {
+			hotspot: true,
+		  },
+	   },
+	  {
+		name: 'body',
+		title: 'Body',
+		type: 'array',
+		of: [
+		  {
+			type: 'block',
+			styles: [
+			  { title: 'Normal', value: 'normal' },
+			  { title: 'Heading 1', value: 'h1' },
+			  { title: 'Heading 2', value: 'h2' },
+			  { title: 'Heading 3', value: 'h3' },
+			  { title: 'Heading 4', value: 'h4' },
+			  { title: 'Heading 5', value: 'h5' },
+			  { title: 'Heading 6', value: 'h6' },
+			  { title: 'Bold', value: 'bold' },
+			  { title: 'Italic', value: 'italic' },
+			],
+			marks: {
+			  decorators: [
+				{ title: 'Strong', value: 'strong' },
+				{ title: 'Emphasis', value: 'em' },
+			  ],
+			},
+		  },
+		],
+	  },
+	  {
+		name: 'imagesGallery',
+		title: 'Image Gallery',
+		type: 'array',
+		of: [{ type: 'image' }]
+	   },
+	  {
+		name: 'body2',
+		title: 'Body2',
+		type: 'array',
+		of: [
+		  {
+			type: 'block',
+			styles: [
+			  { title: 'Normal', value: 'normal' },
+			  { title: 'Heading 1', value: 'h1' },
+			  { title: 'Heading 2', value: 'h2' },
+			  { title: 'Heading 3', value: 'h3' },
+			  { title: 'Heading 4', value: 'h4' },
+			  { title: 'Heading 5', value: 'h5' },
+			  { title: 'Heading 6', value: 'h6' },
+			  { title: 'Bold', value: 'bold' },
+			  { title: 'Italic', value: 'italic' },
+			],
+			marks: {
+			  decorators: [
+				{ title: 'Strong', value: 'strong' },
+				{ title: 'Emphasis', value: 'em' },
+			  ],
+			},
+		  },
+		],
+	  },
+	],
+  };
+  
+
+  
