7f06abf9d03063a0625778b697285f4e98d4cc85
Sina <visualsbysina@gmail.com>
2024-01-22 14:41:11 -0500

    'order' field

--- schemas/caseStudies.js
+++ schemas/caseStudies.js
@@ -1,83 +1,107 @@
 export default {
-	name: 'caseStudies',
-	title: 'Case Studies',
-	type: 'document',
-	fields: [
-	  {
-		name: 'header',
-		title: 'Header',
-		type: 'string',
-	  },
-	  {
-		name: 'image',
-		title: 'Image',
-		type: 'image',
-		options: {
-			hotspot: true,
-		  },
-	   },
-	  {
-		name: 'body',
-		title: 'Body',
-		type: 'array',
-		of: [
-		  {
-			type: 'block',
-			styles: [
-			  { title: 'Normal', value: 'normal' },
-			  { title: 'Heading 1', value: 'h1' },
-			  { title: 'Heading 2', value: 'h2' },
-			  { title: 'Heading 3', value: 'h3' },
-			  { title: 'Heading 4', value: 'h4' },
-			  { title: 'Heading 5', value: 'h5' },
-			  { title: 'Heading 6', value: 'h6' },
-			  { title: 'Bold', value: 'bold' },
-			  { title: 'Italic', value: 'italic' },
-			],
-			marks: {
-			  decorators: [
-				{ title: 'Strong', value: 'strong' },
-				{ title: 'Emphasis', value: 'em' },
-			  ],
-			},
-		  },
-		],
-	  },
-	  {
-		name: 'imagesGallery',
-		title: 'Image Gallery',
-		type: 'array',
-		of: [{ type: 'image' }]
-	   },
-	  {
-		name: 'body2',
-		title: 'Body2',
-		type: 'array',
-		of: [
-		  {
-			type: 'block',
-			styles: [
-			  { title: 'Normal', value: 'normal' },
-			  { title: 'Heading 1', value: 'h1' },
-			  { title: 'Heading 2', value: 'h2' },
-			  { title: 'Heading 3', value: 'h3' },
-			  { title: 'Heading 4', value: 'h4' },
-			  { title: 'Heading 5', value: 'h5' },
-			  { title: 'Heading 6', value: 'h6' },
-			  { title: 'Bold', value: 'bold' },
-			  { title: 'Italic', value: 'italic' },
-			],
-			marks: {
-			  decorators: [
-				{ title: 'Strong', value: 'strong' },
-				{ title: 'Emphasis', value: 'em' },
-			  ],
-			},
-		  },
-		],
-	  },
-	],
-  };
-  
-
-  
+  name: 'caseStudies',
+  title: 'Case Studies',
+  type: 'document',
+  fields: [
+    {
+      name: 'order',
+      title: 'Order',
+      type: 'number',
+      description:
+        'Use this field to set the order of the case studies. Lower numbers appear first.',
+    },
+    {
+      name: 'header',
+      title: 'Header',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Thumbnail Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'featuredImage',
+      title: 'Featured Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+          styles: [
+            {title: 'Normal', value: 'normal'},
+            {title: 'Heading 1', value: 'h1'},
+            {title: 'Heading 2', value: 'h2'},
+            {title: 'Heading 3', value: 'h3'},
+            {title: 'Heading 4', value: 'h4'},
+            {title: 'Heading 5', value: 'h5'},
+            {title: 'Heading 6', value: 'h6'},
+            {title: 'Bold', value: 'bold'},
+            {title: 'Italic', value: 'italic'},
+          ],
+          marks: {
+            decorators: [
+              {title: 'Strong', value: 'strong'},
+              {title: 'Emphasis', value: 'em'},
+            ],
+          },
+        },
+      ],
+    },
+    {
+      name: 'imagesGallery',
+      title: 'Image Gallery',
+      type: 'array',
+      of: [{type: 'image'}],
+    },
+    {
+      name: 'body2',
+      title: 'Body2',
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+          styles: [
+            {title: 'Normal', value: 'normal'},
+            {title: 'Heading 1', value: 'h1'},
+            {title: 'Heading 2', value: 'h2'},
+            {title: 'Heading 3', value: 'h3'},
+            {title: 'Heading 4', value: 'h4'},
+            {title: 'Heading 5', value: 'h5'},
+            {title: 'Heading 6', value: 'h6'},
+            {title: 'Bold', value: 'bold'},
+            {title: 'Italic', value: 'italic'},
+          ],
+          marks: {
+            decorators: [
+              {title: 'Strong', value: 'strong'},
+              {title: 'Emphasis', value: 'em'},
+            ],
+          },
+        },
+      ],
+    },
+  ],
+  orderings: [
+    {
+      title: 'Order, Asc',
+      name: 'orderAsc',
+      by: [{field: 'order', direction: 'asc'}],
+    },
+    {
+      title: 'Order, Desc',
+      name: 'orderDesc',
+      by: [{field: 'order', direction: 'desc'}],
+    },
+  ],
+}
