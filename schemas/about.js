2a290e14f341e414c9f4ca22d801b21d9e6d0988
Sina <visualsbysina@gmail.com>
2024-03-24 22:37:26 -0400

    new about page

--- schemas/about.js
+++ schemas/about.js
@@ -3,79 +3,88 @@ export default {
 	title: 'About',
 	type: 'document',
 	fields: [
-	  {
-		name: 'header',
-		title: 'Header',
-		type: 'string',
-	  },
-	  {
-		name: 'imagesGallery',
-		title: 'Image Gallery',
-		type: 'array',
-		of: [{ type: 'image' }]
-	   },
-	  {
-		name: 'body',
-		title: 'Body',
-		type: 'array',
-		of: [
-		  {
-			type: 'block',
-			styles: [
-			  { title: 'Normal', value: 'normal' },
-			  { title: 'Heading 1', value: 'h1' },
-			  { title: 'Heading 2', value: 'h2' },
-			  { title: 'Heading 3', value: 'h3' },
-			  { title: 'Heading 4', value: 'h4' },
-			  { title: 'Heading 5', value: 'h5' },
-			  { title: 'Heading 6', value: 'h6' },
-			  { title: 'Bold', value: 'bold' },
-			  { title: 'Italic', value: 'italic' },
+		{
+			name: 'header',
+			title: 'Header',
+			type: 'string',
+		},
+		{
+			name: 'logo',
+			title: 'Logo',
+			type: 'array',
+			of: [{type: 'image'}],
+		},
+		{
+			name: 'imagesGallery',
+			title: 'Image Gallery',
+			type: 'array',
+			of: [{type: 'image'}],
+		},
+		{
+			name: 'body',
+			title: 'Body',
+			type: 'array',
+			of: [
+				{
+					type: 'block',
+					styles: [
+						{title: 'Normal', value: 'normal'},
+						{title: 'Heading 1', value: 'h1'},
+						{title: 'Heading 2', value: 'h2'},
+						{title: 'Heading 3', value: 'h3'},
+						{title: 'Heading 4', value: 'h4'},
+						{title: 'Heading 5', value: 'h5'},
+						{title: 'Heading 6', value: 'h6'},
+						{title: 'Bold', value: 'bold'},
+						{title: 'Italic', value: 'italic'},
+						{title: 'Left Align', value: 'left-align'},
+						{title: 'Center Align', value: 'center-align'},
+						{title: 'Right Align', value: 'right-align'},
+					],
+					marks: {
+						decorators: [
+							{title: 'Strong', value: 'strong'},
+							{title: 'Emphasis', value: 'em'},
+						],
+					},
+				},
 			],
-			marks: {
-			  decorators: [
-				{ title: 'Strong', value: 'strong' },
-				{ title: 'Emphasis', value: 'em' },
-			  ],
-			},
-		  },
-		],
-	  },
-	  {
-		name: 'imagesGallery2',
-		title: 'Image Gallery 2',
-		type: 'array',
-		of: [{ type: 'image' }]
-	   },
-	  {
-		name: 'body2',
-		title: 'Body2',
-		type: 'array',
-		of: [
-		  {
-			type: 'block',
-			styles: [
-			  { title: 'Normal', value: 'normal' },
-			  { title: 'Heading 1', value: 'h1' },
-			  { title: 'Heading 2', value: 'h2' },
-			  { title: 'Heading 3', value: 'h3' },
-			  { title: 'Heading 4', value: 'h4' },
-			  { title: 'Heading 5', value: 'h5' },
-			  { title: 'Heading 6', value: 'h6' },
-			  { title: 'Bold', value: 'bold' },
-			  { title: 'Italic', value: 'italic' },
-			],
-			marks: {
-			  decorators: [
-				{ title: 'Strong', value: 'strong' },
-				{ title: 'Emphasis', value: 'em' },
-			  ],
-			},
-		  },
-		],
-	  },
+		},
+		// {
+		// 	name: 'imagesGallery2',
+		// 	title: 'Image Gallery 2',
+		// 	type: 'array',
+		// 	of: [{type: 'image'}],
+		// },
+		// {
+		// 	name: 'body2',
+		// 	title: 'Body2',
+		// 	type: 'array',
+		// 	of: [
+		// 		{
+		// 			type: 'block',
+		// 			styles: [
+		// 				{title: 'Normal', value: 'normal'},
+		// 				{title: 'Heading 1', value: 'h1'},
+		// 				{title: 'Heading 2', value: 'h2'},
+		// 				{title: 'Heading 3', value: 'h3'},
+		// 				{title: 'Heading 4', value: 'h4'},
+		// 				{title: 'Heading 5', value: 'h5'},
+		// 				{title: 'Heading 6', value: 'h6'},
+		// 				{title: 'Bold', value: 'bold'},
+		// 				{title: 'Italic', value: 'italic'},
+		// 				{title: 'Left Align', value: 'left-align'},
+		// 				{title: 'Center Align', value: 'center-align'},
+		// 				{title: 'Right Align', value: 'right-align'},
+		// 			],
+		// 			marks: {
+		// 				decorators: [
+		// 					{title: 'Strong', value: 'strong'},
+		// 					{title: 'Emphasis', value: 'em'},
+		// 				],
+		// 			},
+		// 		},
+		// 	],
+		// },
 	],
-  };
-  
-
-  
+}
--- schemas/caseStudies.js
+++ schemas/caseStudies.js
@@ -30,25 +30,22 @@ export default {
 			layout: 'tags',
 		},
 	},
+	{
+		name: 'thumbnail',
+		title: 'Thumbnail Image',
+		type: 'image',
+		options: {
+		  hotspot: true,
+		},
+	  },
     {
       name: 'image',
-      title: 'Thumbnail Image',
+      title: 'Gif Animation',
       type: 'image',
-      options: {
-        hotspot: true,
-      },
-    },
-    {
-      name: 'featuredImage',
-      title: 'Featured Image',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
     },
     {
       name: 'body',
-      title: 'Body',
+      title: 'Left Side Summary',
       type: 'array',
       of: [
         {
@@ -73,47 +70,48 @@ export default {
         },
       ],
     },
-{
-	name: 'imagesGallery',
-	title: 'Image Gallery',
-	type: 'array',
-	description: 'Add images to the gallery. Gifs must be 800W x 600H',
-	of: [
-		{
-			type: 'image',
-			options: {
-				hotspot: true,
+	{
+		name: 'imageGallery1',
+		title: 'Image Gallery 1',
+		type: 'array',
+		description: 'Add images to the gallery. Gifs must be 800W x 600H',
+		of: [
+			{
+				type: 'image',
+				options: {
+					hotspot: true,
+				},
 			},
-		},
-	],
-},
-    {
-      name: 'body2',
-      title: 'Body2',
-      type: 'array',
-      of: [
-        {
-          type: 'block',
-          styles: [
-            {title: 'Normal', value: 'normal'},
-            {title: 'Heading 1', value: 'h1'},
-            {title: 'Heading 2', value: 'h2'},
-            {title: 'Heading 3', value: 'h3'},
-            {title: 'Heading 4', value: 'h4'},
-            {title: 'Heading 5', value: 'h5'},
-            {title: 'Heading 6', value: 'h6'},
-            {title: 'Bold', value: 'bold'},
-            {title: 'Italic', value: 'italic'},
-          ],
-          marks: {
-            decorators: [
-              {title: 'Strong', value: 'strong'},
-              {title: 'Emphasis', value: 'em'},
-            ],
-          },
-        },
-      ],
-    },
+		],
+	},
+	{
+		name: 'imageGallery2',
+		title: 'Image Gallery 2',
+		type: 'array',
+		description: 'Add images to the gallery. Gifs must be 800W x 600H',
+		of: [
+			{
+				type: 'image',
+				options: {
+					hotspot: true,
+				},
+			},
+		],
+	},
+	{
+		name: 'imageGallery3',
+		title: 'Image Gallery 3',
+		type: 'array',
+		description: 'Add images to the gallery. Gifs must be 800W x 600H',
+		of: [
+			{
+				type: 'image',
+				options: {
+					hotspot: true,
+				},
+			},
+		],
+	},
   ],
   orderings: [
     {
